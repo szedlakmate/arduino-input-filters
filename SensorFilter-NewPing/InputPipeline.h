9c36c6e49e401809a0f7a9b9d71e9e39baa05b2b
Mate Szedlak <szedlakmate@gmail.com>
2025-06-05 20:56:52 -0300

    Add DeadbandGate

--- SensorFilter-NewPing/InputPipeline.h
+++ SensorFilter-NewPing/InputPipeline.h
@@ -4,62 +4,75 @@
 #include "ZeroGuardFilter.h"
 #include "ClampedFilter.h"
 #include "AdaptiveSmoother.h"
+#include "DeadbandGate.h"
 
 /**
  * @brief Composable pipeline that validates and (optionally) filters input signals.
  */
 class InputPipeline {
-    ZeroGuardFilter* zeroGuard;
-    ClampedFilter* clampedGuard;
-    AdaptiveSmoother* smoother;
-    float lastValue;
+  ZeroGuardFilter* zeroGuard;
+  ClampedFilter* clampedGuard;
+  AdaptiveSmoother* smoother;
+  DeadbandGate* deadband;
+  float lastValue;
+
 
 public:
-    InputPipeline()
-      : zeroGuard(nullptr), clampedGuard(nullptr), smoother(nullptr), lastValue(0.0f) {}
+  InputPipeline()
+    : zeroGuard(nullptr), clampedGuard(nullptr), smoother(nullptr), lastValue(0.0f) {}
 
-    void useZeroGuard(int size, int threshold) {
-        if (zeroGuard) delete zeroGuard;
-        zeroGuard = new ZeroGuardFilter(size, threshold);
+  void useZeroGuard(int size, int threshold) {
+    if (zeroGuard) delete zeroGuard;
+    zeroGuard = new ZeroGuardFilter(size, threshold);
+  }
+
+  void useDeadband(float tolerance) {
+    if (deadband) delete deadband;
+    deadband = new DeadbandGate(tolerance);
+  }
+
+  void useClampedGuard(int minValue, int maxValue) {
+    if (clampedGuard) delete clampedGuard;
+    clampedGuard = new ClampedFilter(minValue, maxValue);
+  }
+
+  void useAdaptiveSmoother(float alpha, float jitterThreshold) {
+    if (smoother) delete smoother;
+    smoother = new AdaptiveSmoother(alpha, jitterThreshold);
+  }
+
+  bool update(int input) {
+    if (zeroGuard && !zeroGuard->isValid(input)) return false;
+    if (clampedGuard && !clampedGuard->isValid(input)) return false;
+
+    if (deadband && !deadband->isOutside(input)) {
+      return false;  // input within deadband → hold current value
     }
 
-    void useClampedGuard(int minValue, int maxValue) {
-        if (clampedGuard) delete clampedGuard;
-        clampedGuard = new ClampedFilter(minValue, maxValue);
+    if (smoother) {
+      lastValue = smoother->update(input);
+    } else {
+      lastValue = input;
     }
 
-    void useAdaptiveSmoother(float alpha, float jitterThreshold) {
-        if (smoother) delete smoother;
-        smoother = new AdaptiveSmoother(alpha, jitterThreshold);
-    }
+    return true;
+  }
 
-    bool update(int input) {
-        if (zeroGuard && !zeroGuard->isValid(input)) return false;
-        if (clampedGuard && !clampedGuard->isValid(input)) return false;
+  float get() const {
+    return lastValue;
+  }
 
-        if (smoother) {
-            lastValue = smoother->update(input);
-        } else {
-            lastValue = input;
-        }
+  void reset() {
+    if (smoother) smoother->reset();
+    lastValue = 0.0f;
+  }
 
-        return true;
-    }
-
-    float get() const {
-        return lastValue;
-    }
-
-    void reset() {
-        if (smoother) smoother->reset();
-        lastValue = 0.0f;
-    }
-
-    ~InputPipeline() {
-        if (zeroGuard) delete zeroGuard;
-        if (clampedGuard) delete clampedGuard;
-        if (smoother) delete smoother;
-    }
+  ~InputPipeline() {
+    if (zeroGuard) delete zeroGuard;
+    if (clampedGuard) delete clampedGuard;
+    if (smoother) delete smoother;
+    if (deadband) delete deadband;
+  }
 };
 
 #endif
